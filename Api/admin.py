8036c3cd33948b69386770b0303be44297e1a0fe
simclairsgs <simclair.sgs@gmail.com>
2020-10-30 12:28:34 +0530

    Added Model - products

--- Api/admin.py
+++ Api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Products
 
 # Register your models here.
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['Product_Id', 'product_Name', 'Description', 'MRP' , 'Price', 'Stock_Balance' ,'Discount']
+
+
+admin.site.register(Products, ProductsAdmin)
