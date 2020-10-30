afe502e668f9cae295cac17a665b1856248ea04e
sasikumarnsk <61141651+sasikumarnsk@users.noreply.github.com>
2020-10-30 22:27:06 +0530

    create views urls

--- Api/urls.py
+++ Api/urls.py
@@ -8,5 +8,24 @@ urlpatterns = [
     path('products-add/', views.product_add, name='add-product'),
     path('products-update/<str:Id>/', views.product_update, name='update-product'),
     path('products-delete/<str:Id>/', views.product_delete, name='delete-product'),
-   
+
+    path('login-adduser/',views.Login_Add_Users,name='login-adduser'),
+    path('employees/',views.Employee_list,name='Employee_list'),
+    path('employee-delete/<str:ID>/',views.Employee_Delete,name='employee_delete'),
+    path('employee-update/<str:Id>/', views.Employee_update, name='employee-product'),
+
+
+    path('stock-manage/',views.Stock_Manage,name='stock_manage'),
+    path('view-onestock/<str:ID>/',views.View_One_Stock,name='view_one_stock'),
+    path('view-allstock/',views.View_All_Stock,name='view_all_stock'),
+    path('stock-update/<str:Id>/', views.Stock_update, name='stock-update'),
+
+
+    path('billing-product/',views.Billing_Product,name='billing_product'),
+    path('billing-list/',views.Billing_list,name='billing_list'),
+
+    path('total-sales/',views.Total_Sales,name='total_sales'),
+    path('get-salesof/<str:date>/',views.Get_Sales_Of,name='get_sales_of'),
+    path('get-allsales/',views.Get_All_Sales,name='Get_All_sales'),
+
 ]
--- Api/views.py
+++ Api/views.py
@@ -5,8 +5,8 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Products
-from .serializers import ProductSerializer
+from .models import Products,Login,Sales,Stock,Billing
+from .serializers import ProductSerializer,LoginSerializer,SalesSerializer,StockSerializer,BillingSerializer
 
 
 # Create your views here.
@@ -23,6 +23,21 @@ def product_detail(requests, Id):
 def product_list(request):
         products = Products.objects.all()
         serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+def Employee_list(request):
+        login = Login.objects.all()
+        serializer = LoginSerializer(login, many=True)
+        return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def Billing_list(request):
+        bill = Billing.objects.all()
+        serializer = BillingSerializer(bill, many=True)
         return Response(serializer.data)
 
 @api_view(['POST'])
@@ -42,8 +57,100 @@ def product_update(request, Id):
         return Response("Ok")
     return Response("Failed")
 
+
+@api_view(['POST'])
+def Stock_update(request, Id):
+    stock = Stock.objects.get(Product_Id=Id)
+    serializer =StockSerializer(instance=stock, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response("Ok")
+    return Response("Failed")
+
+
+@api_view(['POST'])
+def Employee_update(request, Id):
+    employee = Login.objects.get(Employee_Id=Id)
+    serializer = LoginSerializer(instance=employee, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response("Ok")
+    return Response("Failed")
+
 @api_view(['DELETE'])
 def product_delete(request, Id):
     products = Products.objects.get(Product_Id=Id)
     products.delete()
     return Response("Deleted Successfully...")
+
+@api_view(['POST'])
+def Login_Add_Users(request):
+    serializer=LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response("OK")
+    return Response("Failed")
+
+@api_view(['DELETE'])
+def Employee_Delete(request,ID):
+    Employee=Login.objects.get(Employee_Id=ID)
+    Employee.delete()
+    return Response('Deleted Successfully')
+
+@api_view(['POST'])
+def Stock_Manage(request):
+    serializer=StockSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response('Successfully')
+    return Response('Failed')
+
+
+@api_view(['GET'])
+def View_One_Stock(request,ID):
+    stock=Stock.objects.get(Product_Id=ID)
+    serializer=StockSerializer(stock,many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def View_All_Stock(request):
+    stock=Stock.objects.all()
+    serializer=StockSerializer(stock,many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def Billing_Product(request):
+    serializer=BillingSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response("Successfully")
+    return Response("Failed")
+
+
+@api_view(['POST'])
+def Total_Sales(request):
+    Sales=Billing.objects.get.all()
+    serializer = SalesSerializer(instance=Sales,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response("Ok")
+    return Response("Failed")
+
+@api_view(['GET'])
+def Get_Sales_Of(request,date):
+    sales=Sales.objects.get(Date=date)
+    serializer=SalesSerializer(sales,many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def Get_All_Sales(request):
+    #sales=Sales.objects.get.all()
+    alls=Sales.objects.all()
+    serializer=SalesSerializer(alls,many=True)
+    return Response(serializer.data)
+    
+
+
+
