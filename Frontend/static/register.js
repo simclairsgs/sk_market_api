963213d54fea15a278b25500cfcfcf39a9165a52
simclairsgs <simclair.sgs@gmail.com>
2020-11-03 21:14:38 +0530

    Register works done

--- Frontend/static/register.js
+++ Frontend/static/register.js
@@ -8,11 +8,26 @@ function register()
    let remployee_addr = document.getElementById("remp_address").value;
    let remployee_mail = document.getElementById("remp_email").value;
    let employee_doj = new Date().toDateString()
-   
-   //post to api
-    $.post('/api/register-user/',"name="+remployee_name+"&pass="+
-    remployee_pass+"&num="+remployee_num+"&dob="+remployee_dob+
-    "&addr="+remployee_addr+"&mail="+remployee_mail+"&doj="+employee_doj,
-    function(status,data){ alert(status,data)})
+   // verify data
+   if(remployee_pass.length < 8)
+   {
+       toastr.error("Password too short");
+   }
+   else
+   {
+        //post to api
+        $.post('/api/register-user/',"name="+remployee_name+"&pass="+
+        remployee_pass+"&num="+remployee_num+"&dob="+remployee_dob+
+        "&addr="+remployee_addr+"&mail="+remployee_mail+"&doj="+employee_doj,
+        function(data,status){
+            if(status=="success")
+            {
+                alert("Your Employee Id is\n"+data.Employee_Id)
+                window.location="/"
+            }
+            
+            })
 
+   }
+   
 }
