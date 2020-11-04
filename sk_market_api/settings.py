7bdbeb6b3ceb0d70f6707484700ea271cc8ad720
simclairsgs <simclair.sgs@gmail.com>
2020-11-04 20:33:34 +0530

    dynamic structure with main iframe

--- sk_market_api/settings.py
+++ sk_market_api/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+X_FRAME_OPTIONS = 'ALLOWALL'
+
+XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
